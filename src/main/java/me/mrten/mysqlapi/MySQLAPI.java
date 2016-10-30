bde10daf77fb71706f1b06543977b41835172322
Mrten <maarten@zuidhoorn.com>
2016-10-30 22:22:43 +0100

    Add Javadoc comment

--- src/main/java/me/mrten/mysqlapi/MySQLAPI.java
+++ src/main/java/me/mrten/mysqlapi/MySQLAPI.java
@@ -2,6 +2,9 @@ package me.mrten.mysqlapi;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * This class exists so that Spigot can load the library as a plugin
+ */
 public class MySQLAPI extends JavaPlugin {
 
 }
